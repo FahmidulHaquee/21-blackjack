9d48e6e7b2d05387f17c9f64d3fa6ed98ed1f28c
FamHaque <fahmidulhaque1997@gmail.com>
2024-08-05 20:08:29 +0100

    remove c#

--- Javascript/Class-based/blackjack.js
+++ Javascript/Class-based/blackjack.js
@@ -1,98 +1,213 @@
-import { getDefaultLogger } from "./support/logging.ts";
+// deno-lint-ignore-file no-fallthrough
 
-class Game {
-  deck = [];
-  playersHand = [];
-  dealersHand = [];
-  playersPoints = [];
-  dealersPoints = [];
-  LOSE_MESSAGE = "You lose!";
-  WIN_MESSAGE = "You win!";
-  DRAW_MESSAGE = "Draw!";
-  defaultLogger = await getDefaultLogger();
+const LOSE_MESSAGE = "You lose!"
+const WIN_MESSAGE = "You win!"
+const DRAW_MESSAGE = "Draw!"
 
-  createDeck() {
-    const cards = [];
-    const suits = ["D", "C", "S", "H"];
-    const numbers = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-    ];
-
-    for (let i in suits) {
-      for (let j in numbers) {
-        let currentCard = numbers[j] + suits[i];
-        cards.push(currentCard);
-      }
-    }
-    this.deck = cards;
+export class Card {
+  constructor(rank, suit) {
+    this.rank = rank
+    this.suit = suit.toUpperCase()
+    this.points = this.cardPoints()
   }
 
-  shuffleDeck(seed = 1) {
-    let currentIndex = this.deck.length;
-    let temporaryValue, randomIndex;
+  toString() {
+    return `${this.rank}${this.suit}`
+  }
 
-    let random = () => {
-      var x = Math.sin(seed++) * 10000;
-      return x - Math.floor(x);
+  cardPoints() {
+    const ace = 'A'
+    const cardsScore10 = ['J', 'Q', 'K']
+    const numberCards = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+    if (numberCards.includes(this.rank)) 
+      this.points += parseInt(this.rank)
+    else if (cardsScore10.includes(this.rank)) 
+      this.points += 10
+    else if (this.rank === ace)
+      this.points += 11
+  }
+}
+
+export class Hand {
+  constructor(cards) {
+    if (!cards.every((card) => card instanceof Card))
+      throw new TypeError('A Hand can only contain Cards')
+    this.cards = cards
+    this.numberOfCards = cards.length
+    this.points = this.handPoints()
+  }
+
+  handPoints() {
+    if (this.numberOfCards > 5) 
+      this.points += 21
+    else if (this.numberOfCards === 0) 
+      throw new Error('Can\'t calculate points for empty hand!')
+    else 
+      this.cards.forEach(card => this.cardPoints(card))
+  }
+
+  cardPoints() {
+    this.points += this.cards.card.points
+  }
+}
+
+export class Deck {
+  constructor() {
+    this.suits = ['D', 'C', 'S', 'H']
+    this.ranks = [
+      'A',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'J',
+      'Q',
+      'K'
+    ]
+    this.cards = this.createDeck()
+  }
+
+  createDeck() {
+    const newCards = []
+    for (const suit of this.suits) {
+      for (const rank of this.ranks) {
+        const card = rank + suit
+        newCards.push(card)
+      }
+    }
+    return newCards
+  }
+
+  draw() {
+    const card = this.cards.shift()
+    const rank = card[0]
+    const suit = card[1]
+    return new Card(rank, suit)
+  }
+
+  shuffle(seed = 1) {
+    let currentIndex = this.cards.length
+    let temporaryValue, randomIndex
+
+    const random = () => {
+      const x = Math.sin(seed++) * 10000
+      return x - Math.floor(x)
     };
 
     while (0 !== currentIndex) {
-      randomIndex = Math.floor(random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = this.deck[currentIndex];
-      this.deck[currentIndex] = this.deck[randomIndex];
-      this.deck[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(random() * currentIndex)
+      currentIndex -= 1
+      temporaryValue = this.cards[currentIndex]
+      this.cards[currentIndex] = this.cards[randomIndex]
+      this.cards[randomIndex] = temporaryValue
     }
   }
-
-  pointsFor(hand) {
-    const ace = "A";
-    const cardsScore10 = ["J", "Q", "K"];
-    const numberCards = ["2", "3", "4", "5", "6", "7", "8", "9"];
-
-    let points = 0;
-
-    if (hand.length >= 6) {
-      points += 21;
-    } else if (hand[0][0] && hand[1][0] === "A") {
-      points += 21;
-    } else {
-      for (let card of hand) {
-        if (card.slice(0, 2) === "10") {
-          points += 10;
-        } else if (card.includes(ace)) {
-          points += 11;
-        } else if (cardsScore10.includes(card[0])) {
-          points += 10;
-        } else if (numberCards.includes(card[0])) {
-          points += parseInt(card[0]);
-        }
-      }
-    }
-    return points;
-  }
-
-  playerTurn()
-
-  dealerTurn()
-
-  decideVictor()
 }
 
-const newGame = new Game();
-newGame.createDeck();
-console.log(newGame.deck);
-newGame.shuffleDeck();
-console.log(newGame.deck);
+class Player {
+  constructor() {
+    this.continueTurn = true;
+    this.didPlayer21 = false;
+    this.didPlayerBust = false;
+  }
+}
+
+const deck = new Deck()
+deck.shuffle()
+
+const player = new Player()
+console.log('Player\'s turn first')
+const playersHand = new Hand([deck.draw(), deck.draw()])
+console.log(`Your hand is ${playersHand.cards}\n(${pointsFor(playersHand.points)} points)\n`)
+
+while (player.continueTurn) {
+  const action = window.prompt("What do you want to do? (hit or stick)\n")
+
+  switch (action) {
+    case "hit": {
+      console.log("Hitting");
+      playersHand.cards.push(deck.draw());
+      console.log(
+        `Your hand is ${playersHand.cards}\n(${pointsFor(
+          playersHand.points
+        )} points)\n`
+      );
+      if (playersHand.points == 21) {
+        console.log(`You have 21.\n`);
+        player.continueTurn = false
+        player.didPlayerBust = false
+        player.didPlayer21 = true
+      } 
+      else if (playerPoints < 21) {
+        console.log("You have less than 21, and can make another move.\n")
+        player.continueTurn = true
+        player.didPlayerBust = false
+        player.didPlayer21 = false
+      } 
+      else if (playerPoints > 21) {
+        console.log("You went over 21!\n");
+        player.continueTurn = false
+        player.didPlayerBust = true
+        player.didPlayer21 = false
+      }
+    }
+    case "stick": {
+      console.log("Sticking\n");
+      player.continueTurn = false
+    }
+    default: {
+      console.log("Command not recognised. Type Hit or Stick\n");
+      player.continueTurn = true
+    }
+  }
+}
+
+if (player.didPlayerBust)
+  throw new Error(LOSE_MESSAGE)
+
+const dealer = new Player()
+console.log('Dealer\'s turn now')
+const dealersHand = new Hand([deck.draw(), deck.draw()])
+console.log(`Dealer's hand is ${dealersHand.cards}\n(${dealersHand.points} points)\n`)
+
+while (dealer.continueTurn) {
+  let action = ''
+  if (dealerPoints < 17)
+    action = "hit";
+  else if (dealerPoints >= 17 && dealerPoints <= 21)
+    action = "stick";
+  else if (dealerPoints > 21) 
+    throw new Error("Dealer has busted\n", WIN_MESSAGE)
+  
+  switch (action) {
+    case "hit": {
+      console.log("Dealer is hitting\n");
+      dealersHand(deck.draw());
+      dealer.continueTurn = true
+      dealer.didPlayer21 = false
+      dealer.didPlayerBust = false
+    }
+    case "stick": {
+      console.log("Dealer is sticking");
+      console.log(`Dealer ends this round with a hand of ${dealersHand.cards}\n With ${dealersHand.points} points`)
+      dealer.continueTurn = false
+      dealer.didPlayer21 = false
+      dealer.didDealerBust = false
+    }
+  }
+}
+
+if (dealer.didPlayerBust)
+  throw new Error(WIN_MESSAGE)
+else if (playersHand.points > dealersHand.points) 
+  throw new Error(WIN_MESSAGE)
+else if (playersHand.points == dealersHand.points) 
+  throw new Error(DRAW_MESSAGE);
+else 
+  throw new Error(LOSE_MESSAGE);
